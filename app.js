179b230472e3c29ecb31978bb099d74989394179
AungPyaeOo541378 <aungpyaeoo.541378@gmail.com>
2025-01-19 16:13:05 +0900

    commit

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var one = document.querySelector("#one");
 var two = document.querySelector("#two");
 var three = document.querySelector("#three");
+var four = document.querySelector("#four");
+var five = document.querySelector("#five");
+var six = document.querySelector("#six");
+var seven = document.querySelector("#seven");
+var eight = document.querySelector("#eight");
+var nine = document.querySelector("#nine");
 
 var plus = document.querySelector("#plus");
 var equal = document.querySelector("#equal");
@@ -20,6 +26,31 @@ two.addEventListener("click", function () {
 three.addEventListener("click", function () {
   answerNum.value += three.innerHTML; //3
 });
+
+four.addEventListener("click", function () {
+  answerNum.value += four.innerHTML;  //4  
+})
+
+five.addEventListener("click",function () {
+  answerNum.value += five.innerHTML;
+})
+
+six.addEventListener("click", function () {
+  answerNum.value += six.innerHTML;
+})
+
+seven.addEventListener("click", function () {
+  answerNum.value += seven.innerHTML;
+})
+
+eight.addEventListener("click", function () {
+  answerNum.value += eight.innerHTML;
+})
+
+nine.addEventListener("click", function () {
+  answerNum.value += nine.innerHTML;
+})
+
 
 plus.addEventListener("click", function () {
   tempOne = answerNum.value; //123
